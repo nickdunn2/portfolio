90263c01034d5e670e90c7740b2c3d6318ae6651
Nick Dunn <nick.dunn@flosports.tv>
2024-04-22 07:07:59 -0500

    feat(about): introduce SectionHeader, About components

--- components/intro.tsx
+++ components/intro.tsx
@@ -44,7 +44,7 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -54,7 +54,7 @@ const Intro = () => {
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites, apps and APIs</span>. My focus is on{" "}
         <span className="underline">JavaScript & TypeScript</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
