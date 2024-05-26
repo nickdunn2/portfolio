60fff935394ce80538cbb36e0aca6a1e24c7a1ca
Nick Dunn <nick.dunn@flosports.tv>
2024-05-26 10:22:17 -0500

    refactor(email): create utility function to render error as string

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,6 +1,6 @@
 'use server'
 import { Resend } from "resend"
-import { validateString } from "@/lib/utils"
+import * as UtilFunctions from "@/lib/utils"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -8,13 +8,13 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get('senderEmail')
   const message = formData.get('message')
 
-  if (!validateString(senderEmail, 500)) {
+  if (!UtilFunctions.validateString(senderEmail, 500)) {
     return {
       error: 'Invalid email',
     }
   }
 
-  if (!validateString(message, 5000)) {
+  if (!UtilFunctions.validateString(message, 5000)) {
     return {
       error: 'Invalid message',
     }
@@ -22,14 +22,15 @@ export const sendEmail = async (formData: FormData) => {
 
   try {
     await resend.emails.send({
-      from: "Portfolio Contact Form <onboarding@resend.blow>",
+      from: "Portfolio Contact Form <onboarding@resend.dev>",
       to: 'nickdunn2@gmail.com',
       subject: 'New message from your portfolio',
       reply_to: senderEmail,
       text: message,
     })
   } catch (error: unknown) {
-    // TODO: add more robust error handling.
-    console.log(error)
+    return {
+      error: UtilFunctions.getErrorMessage(error),
+    }
   }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,18 @@
 export const validateString = (value: unknown, maxLength: number): value is string => {
   return !!value && typeof value === 'string' && value.length <= maxLength
 }
+
+/**
+ * Function to return a string error message from an unknown error object.
+ */
+export const getErrorMessage = (error: unknown): string => {
+  if (error instanceof Error) {
+    return error.message
+  } else if (!!error && typeof error === 'object' && 'message' in error) {
+    return String(error.message)
+  } else if (typeof error === 'string') {
+    return error
+  } else {
+    return 'Whoops! An unknown error occurred. Please try again.'
+  }
+}
