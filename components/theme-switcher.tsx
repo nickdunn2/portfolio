19473a5c60b2f295d2f0f61dbe00cd447a742bf1
Nick Dunn <nick.dunn@flosports.tv>
2024-05-27 10:00:14 -0500

    feat(theme-switcher): toggle icon based on theme

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -1,14 +1,25 @@
-import { BsSun } from "react-icons/bs"
+'use client'
 
+import { BsMoon, BsSun } from "react-icons/bs"
+import { useState } from "react"
+
+type Theme = 'light' | 'dark'
 
 const ThemeSwitcher = () => {
+  const [theme, setTheme] = useState<Theme>('light')
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
   return (
     <button
       className="fixed flex items-center justify-center bottom-5 right-5 rounded-full bg-white bg-opacity-80
       w-[3rem] h-[3rem] backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl
       hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
     >
-      <BsSun/>
+      {theme === 'light' ? <BsSun/> : <BsMoon/>}
     </button>
   )
 }
