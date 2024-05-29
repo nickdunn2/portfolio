680655930ad386c22be2124e780ff3c8ba37be1f
Nick Dunn <nick.dunn@flosports.tv>
2024-05-29 09:12:28 -0500

    fix(projects): remove images and extra marging/padding on mobile

--- components/project.tsx
+++ components/project.tsx
@@ -28,9 +28,10 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
     >
       <section
         className="relative bg-gray-100 max-w-[42rem] rounded-lg border border-black/5 overflow-hidden sm:pr-8 sm:h-[20rem]
-        group-even:pl-8 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white-20"
+        sm:group-even:pl-8 hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white-20"
       >
-        <div className="flex flex-col px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full group-even:ml-[18rem]">
+        <div className="flex flex-col px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full
+        sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -47,17 +48,17 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
           src={imageUrl}
           alt="project I worked on"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-        transition
-        group-even:right-[initial]
-        group-even:-left-40
-        group-hover:scale-105
-        group-hover:-translate-x-3
-        group-hover:translate-y-3
-        group-hover:-rotate-2
-        group-even:group-hover:translate-x-3
-        group-even:group-hover:translate-y-3
-        group-even:group-hover:rotate-2"
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+            transition
+            group-even:right-[initial]
+            group-even:-left-40
+            group-hover:scale-105
+            group-hover:-translate-x-3
+            group-hover:translate-y-3
+            group-hover:-rotate-2
+            group-even:group-hover:translate-x-3
+            group-even:group-hover:translate-y-3
+            group-even:group-hover:rotate-2"
         />
       </section>
     </motion.div>
